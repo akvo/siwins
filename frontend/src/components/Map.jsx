cdfdd34f4d87bfc6e014d7fa2f500bb0b46ae25b
wayangalihpratama <wgprtm@gmail.com>
2022-12-05 10:15:40 +0800

    [#5] Add axios & load marker on maps

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  GeoJSON,
+  TileLayer,
+  Circle,
+  Tooltip,
+} from "react-leaflet";
 import { defaultPos, geojson, tileOSM } from "../util/geo-util";
+import { api } from "../lib";
 
 const defPos = defaultPos();
+
+const Markers = ({ data }) => {
+  data = data.filter((d) => d.geo);
+  return data.map(({ id, geo, name }) => {
+    const hovered = false;
+    const fill = "#F00";
+    const r = 3;
+    const stroke = "#fff";
+    return (
+      <Circle
+        key={id}
+        center={geo}
+        pathOptions={{
+          fillColor: hovered ? "#FFF" : fill,
+          color: fill,
+          opacity: 1,
+          fillOpacity: 1,
+        }}
+        radius={r * (hovered ? 3 : 1)}
+        stroke={stroke}
+      >
+        <Tooltip direction="top">{name}</Tooltip>
+      </Circle>
+    );
+  });
+};
 
 const Map = () => {
   // use tile layer from config
   const baseMap = tileOSM;
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    api
+      .get("/data/maps")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((e) => console.error(e))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div className="map-container">
       <MapContainer
         bounds={defPos.bbox}
         zoomControl={false}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         style={{
           height: "100%",
           width: "100%",
@@ -24,14 +70,15 @@ const Map = () => {
         <GeoJSON
           key="geodata"
           style={{
-            weight: 2,
+            weight: 1,
             fillColor: "#00989f",
-            fillOpacity: 0.5,
-            opacity: 0.5,
+            fillOpacity: 0.25,
+            opacity: 0.25,
             color: "#FFF",
           }}
           data={geojson}
         />
+        {!loading && <Markers data={data} />}
       </MapContainer>
     </div>
   );
