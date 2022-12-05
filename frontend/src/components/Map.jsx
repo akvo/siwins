e586021f72d7860e2353125d34f598e35683610c
wayangalihpratama <wgprtm@gmail.com>
2022-12-05 12:41:55 +0800

    [#5] Load monitoring chart when a point clicked

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -9,10 +9,87 @@ import {
 } from "react-leaflet";
 import { defaultPos, geojson, tileOSM } from "../util/geo-util";
 import { api } from "../lib";
+import { Modal, Spin, Collapse } from "antd";
+import { Column } from "@ant-design/plots";
+import { chain, groupBy, orderBy } from "lodash";
 
 const defPos = defaultPos();
 
-const Markers = ({ data }) => {
+const { Panel } = Collapse;
+
+const RenderChart = ({ data }) => {
+  // get sample data to find question type
+  const sample = data?.[0] || {};
+  const qtype = sample?.type;
+  data = orderBy(
+    data.map((d) => ({
+      value: d.value,
+      date: d.date,
+    })),
+    ["date"],
+    "desc"
+  );
+  let config = {
+    data,
+    // legend: {
+    //   position: "top-left",
+    // },
+  };
+  switch (qtype) {
+    case "option":
+      return null;
+    default:
+      config = {
+        ...config,
+        xField: "date",
+        yField: "value",
+        label: {
+          position: "middle",
+          // 'top', 'bottom', 'middle',
+          style: {
+            fill: "#FFFFFF",
+            opacity: 0.6,
+          },
+        },
+        xAxis: {
+          label: {
+            autoHide: true,
+            autoRotate: false,
+          },
+        },
+      };
+      return <Column {...config} />;
+  }
+};
+
+const Charts = ({ chartData }) => {
+  const [activePanel, setActivePanel] = useState(1);
+  const { monitoring } = chartData;
+  const grouped = chain(
+    groupBy(
+      monitoring.filter((d) => d.type === "number"),
+      "question"
+    )
+  ).value();
+
+  return (
+    <Collapse
+      defaultActiveKey={[activePanel]}
+      onChange={setActivePanel}
+      accordion={true}
+    >
+      {Object.keys(grouped).map((key, ki) => {
+        return (
+          <Panel header={key} key={ki + 1}>
+            <RenderChart data={grouped?.[key] || []} />
+          </Panel>
+        );
+      })}
+    </Collapse>
+  );
+};
+
+const Markers = ({ data, getChartData }) => {
   data = data.filter((d) => d.geo);
   return data.map(({ id, geo, name }) => {
     const hovered = false;
@@ -31,6 +108,7 @@ const Markers = ({ data }) => {
         }}
         radius={r * (hovered ? 3 : 1)}
         stroke={stroke}
+        eventHandlers={{ click: () => getChartData(id) }}
       >
         <Tooltip direction="top">{name}</Tooltip>
       </Circle>
@@ -43,6 +121,8 @@ const Map = () => {
   const baseMap = tileOSM;
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [selectedPoint, setSelectedPoint] = useState(null);
+  const [chartData, setChartData] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -55,32 +135,61 @@ const Map = () => {
       .finally(() => setLoading(false));
   }, []);
 
+  const getChartData = (id) => {
+    setSelectedPoint(data.find((d) => d.id === id));
+    api
+      .get(`/data/chart/${id}`)
+      .then((res) => {
+        setChartData(res.data);
+      })
+      .catch((e) => console.error(e));
+  };
+
   return (
-    <div className="map-container">
-      <MapContainer
-        bounds={defPos.bbox}
-        zoomControl={false}
-        scrollWheelZoom={true}
-        style={{
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <TileLayer {...baseMap} />
-        <GeoJSON
-          key="geodata"
+    <>
+      <div className="map-container">
+        <MapContainer
+          bounds={defPos.bbox}
+          zoomControl={false}
+          scrollWheelZoom={true}
           style={{
-            weight: 1,
-            fillColor: "#00989f",
-            fillOpacity: 0.25,
-            opacity: 0.25,
-            color: "#FFF",
+            height: "100%",
+            width: "100%",
           }}
-          data={geojson}
-        />
-        {!loading && <Markers data={data} />}
-      </MapContainer>
-    </div>
+        >
+          <TileLayer {...baseMap} />
+          <GeoJSON
+            key="geodata"
+            style={{
+              weight: 1,
+              fillColor: "#00989f",
+              fillOpacity: 0.25,
+              opacity: 0.25,
+              color: "#FFF",
+            }}
+            data={geojson}
+          />
+          {!loading && <Markers data={data} getChartData={getChartData} />}
+        </MapContainer>
+      </div>
+
+      {/* Chart Modal */}
+      <Modal
+        title={selectedPoint?.name}
+        open={selectedPoint}
+        onCancel={() => setSelectedPoint(null)}
+        footer={null}
+        width={650}
+      >
+        {!chartData ? (
+          <div className="loading-wrapper">
+            <Spin />
+          </div>
+        ) : (
+          <Charts chartData={chartData} />
+        )}
+      </Modal>
+    </>
   );
 };
 
