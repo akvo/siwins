25434e0e1dcf31bc076528a75e3d3390e0c8114b
wayangalihpratama <wgprtm@gmail.com>
2022-12-06 16:15:40 +0800

    [#3] Update sync test to check if older data moved to history

--- backend/seeder/data_sync.py
+++ backend/seeder/data_sync.py
@@ -29,6 +29,7 @@ def data_sync(token: dict, session: Session, sync_data: dict):
         crud_data.delete_bulk(session=session, ids=deleted_data_ids)
         print(f"Sync | Delete Datapoints: {deleted_data_ids}")
     # manage form instance changes
+    # TODO:: Need to sort value by createdAt?
     for fi in changes.get('formInstanceChanged'):
         form = crud_form.get_form_by_id(
             session=session, id=fi.get('formId'))
--- backend/tests/test_010_seed_and_sync.py
+++ backend/tests/test_010_seed_and_sync.py
@@ -10,6 +10,7 @@ from seeder.datapoint import datapoint_seeder
 from seeder.data_sync import data_sync
 from db import crud_sync
 from db import crud_form
+from db import crud_data
 
 sys.path.append("..")
 pytestmark = pytest.mark.asyncio
@@ -43,6 +44,106 @@ class TestSeedAndSync():
             assert check_form.id == fid
 
     @pytest.mark.asyncio
+    async def test_get_data(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
+        # registration data
+        data = crud_data.get_all_data(session=session, registration=True)
+        data = [d.serialize for d in data]
+        assert data == [{
+            "id": 1,
+            "datapoint_id": 716330915,
+            "identifier": "dfmn-hw5g-11se",
+            "name": "SMA N 1 Nusa Penida - High school",
+            "form": 733030972,
+            "registration": True,
+            "geo": {
+                "lat": -8.676368333333333,
+                "long": 115.49182166666667
+            },
+            "created": "December 01, 2022",
+            "updated": "December 01, 2022",
+            "answer": [{
+                "question": 718001069,
+                "value": "High school"
+            }, {
+                "question": 721880978,
+                "value": "-8.676368333333333|115.49182166666667"
+            }, {
+                "question": 738940972,
+                "value": "SMA N 1 Nusa Penida"
+            }]
+        }]
+        # monitoring data
+        temp_data = crud_data.get_all_data(session=session, registration=False)
+        data = [d.serialize for d in temp_data]
+        assert data == [{
+            "id": 2,
+            "datapoint_id": 716330915,
+            "identifier": "dfmn-hw5g-11se",
+            "name": "SMA N 1 Nusa Penida - High school",
+            "form": 729240983,
+            "registration": False,
+            "geo": None,
+            "created": "December 01, 2022",
+            "updated": "December 01, 2022",
+            "answer": [
+                {"question": 725310914, "value": 225.0},
+                {"question": 735090984, "value": 15.0},
+                {"question": 738950915, "value": "Clean"},
+            ]
+        }]
+        # monitoring format
+        data = [d.to_monitoring_data for d in temp_data]
+        assert data == [{
+            "id": 2,
+            "name": "SMA N 1 Nusa Penida - High school",
+            "monitoring": [{
+                "history": False,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 225.0,
+            }, {
+                "history": False,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 15.0,
+            }, {
+                "history": False,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "option",
+                "value": "Clean",
+            }, {
+                "history": True,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 200.0,
+            }, {
+                "history": True,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 10.0,
+            }, {
+                "history": True,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "option",
+                "value": "Clean",
+            }]
+        }]
+
+    @pytest.mark.asyncio
     async def test_sync(
         self, app: FastAPI, session: Session, client: AsyncClient
     ) -> None:
@@ -52,3 +153,96 @@ class TestSeedAndSync():
         with open(sync_data_file) as json_file:
             sync_data = json.load(json_file)
         data_sync(token=token, session=session, sync_data=sync_data)
+        # monitoring data
+        temp_data = crud_data.get_all_data(session=session, registration=False)
+        # monitoring format
+        data = [d.to_monitoring_data for d in temp_data]
+        assert data == [{
+            "id": 2,
+            "name": "SMA N 1 Nusa Penida - High school",
+            "monitoring": [{
+                "history": False,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 02, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 100.0,
+            }, {
+                "history": False,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 02, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 10.0,
+            }, {
+                "history": False,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 02, 2022 - 3:03:25 AM",
+                "type": "option",
+                "value": "Clean",
+            }, {
+                "history": True,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 02, 2022 - 3:02:59 AM",
+                "type": "option",
+                "value": "Clean",
+            }, {
+                "history": True,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 02, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 110.0,
+            }, {
+                "history": True,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 02, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 11.0,
+            }, {
+                "history": True,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 15.0,
+            }, {
+                "history": True,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "number",
+                "value": 225.0,
+            }, {
+                "history": True,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 01, 2022 - 3:03:25 AM",
+                "type": "option",
+                "value": "Clean",
+            }, {
+                "history": True,
+                "question_id": 725310914,
+                "question": "Number students",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 200.0,
+            }, {
+                "history": True,
+                "question_id": 738950915,
+                "question": "Status of toilet",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "option",
+                "value": "Clean",
+            }, {
+                "history": True,
+                "question_id": 735090984,
+                "question": "Number toilets",
+                "date": "Dec 01, 2022 - 3:02:59 AM",
+                "type": "number",
+                "value": 10.0,
+            }]
+        }]
