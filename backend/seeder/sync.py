6b7c1ab8088b9bc7a95d663fe267e58207bb62a1
wayangalihpratama <wgprtm@gmail.com>
2022-12-06 15:49:11 +0800

    [#3] Create test for sync API

--- backend/seeder/sync.py
+++ backend/seeder/sync.py
@@ -4,149 +4,13 @@ import flow.auth as flow_auth
 from datetime import timedelta
 from db.connection import Base, SessionLocal, engine
 from db import crud_sync
-from db import crud_data
-from db import crud_question
-from db import crud_answer
-from db import crud_form
-from models.question import QuestionType
-from models.answer import Answer
-from models.history import History
+from seeder.data_sync import data_sync
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 Base.metadata.create_all(bind=engine)
 session = SessionLocal()
 
 start_time = time.process_time()
-
-
-def sync(sync_data: dict):
-    # TODO:: Support other changes from FLOW API
-    print("------------------------------------------")
-    changes = sync_data.get('changes')
-    next_sync_url = sync_data.get('nextSyncUrl')
-    # save next sync URL
-    if next_sync_url:
-        crud_sync.add_sync(session=session, url=next_sync_url)
-    # manage deleted datapoint
-    if changes.get('dataPointDeleted'):
-        deleted_data_ids = [int(dpid) for dpid
-                            in changes.get('dataPointDeleted')]
-        crud_data.delete_bulk(session=session, ids=deleted_data_ids)
-        print(f"Sync | Delete Datapoints: {deleted_data_ids}")
-    # manage form instance changes
-    for fi in changes.get('formInstanceChanged'):
-        form = crud_form.get_form_by_id(
-            session=session, id=fi.get('formId'))
-        if not form:
-            continue
-        datapoint_id = fi.get('dataPointId')
-        answers = []
-        geoVal = None
-        monitoring = True if form.registration_form else False
-        # check if monitoring datapoint exist
-        datapoint_exist = False
-        if monitoring:
-            datapoint_exist = crud_data.get_data_by_identifier(
-                session=session,
-                identifier=fi.get('identifier'),
-                form=form.id)
-        current_data = crud_data.get_data_by_datapoint_id(
-            session=session,
-            datapoint_id=datapoint_id,
-            form=form.id)
-        # fetching answers value into answer model
-        for key, value in fi.get('responses').items():
-            for val in value:
-                for kval, aval in val.items():
-                    question = crud_question.get_question_by_id(
-                        session=session, id=kval)
-                    if not question:
-                        print(f"{kval}: 404 not found")
-                        continue
-                    if question.type == QuestionType.geo:
-                        geoVal = [aval.get('lat'), aval.get('long')]
-                    answer = Answer(
-                        data=datapoint_id,
-                        question=question.id,
-                        created=fi.get('createdAt'))
-                    # if datapoint exist, move current answer as history
-                    if datapoint_exist:
-                        print(f"Sync | Update Datapoint {datapoint_exist.id}")
-                        current_answers = \
-                            crud_answer.get_answer_by_data_and_question(
-                                session=session,
-                                data=datapoint_exist.id,
-                                questions=[question.id])
-                        if len(current_answers):
-                            # create history and update current answer
-                            current_answer = current_answers[0]
-                            # create history
-                            history = History(
-                                question=question.id,
-                                data=datapoint_exist.id,
-                                text=current_answer.text,
-                                value=current_answer.value,
-                                options=current_answer.options,
-                                created=current_answer.created,
-                                updated=current_answer.updated)
-                            # update current answer
-                            update_answer = answer
-                            update_answer.id = current_answer.id,
-                            update_answer.data = datapoint_exist.id,
-                            crud_answer.update_answer(
-                                session=session, answer=update_answer,
-                                history=history, type=question.type,
-                                value=aval)
-                            print(f"Sync | Update Answer {answer.id}")
-                        else:
-                            # add answer
-                            new_answer = answer
-                            new_answer.data = datapoint_exist.id
-                            crud_answer.add_answer(
-                                session=session, answer=new_answer,
-                                type=question.type, value=aval)
-                            print(f"Sync | New Answer {answer.id}")
-                    # update registration datapoint
-                    if current_data and not datapoint_exist:
-                        crud_answer.update_answer(
-                            session=session, answer=answer,
-                            type=question.type, value=aval)
-                    # new datapoint and answers
-                    if not datapoint_exist and not current_data:
-                        answer = crud_answer.append_value(
-                            answer=answer, value=aval, type=question.type)
-                        answers.append(answer)
-        if not current_data and not datapoint_exist:
-            # add new datapoint
-            data = crud_data.add_data(
-                session=session,
-                datapoint_id=datapoint_id,
-                identifier=fi.get('identifier'),
-                name=fi.get('displayName'),
-                form=form.id,
-                registration=False if monitoring else True,
-                geo=geoVal,
-                created=fi.get('createdAt'),
-                answers=answers)
-            print(f"Sync | New Datapoint: {data.id}")
-            continue
-        if current_data:
-            # update datapoint
-            update_data = current_data
-            update_data.name = fi.get('displayName'),
-            update_data.form = form.id,
-            update_data.geo = geoVal,
-            update_data.updated = fi.get('modifiedAt'),
-            updated = crud_data.update_data(
-                session=session, data=update_data)
-            print(f"Sync | Update Datapoint: {updated.id}")
-            continue
-    print("------------------------------------------")
-    # call next sync URL
-    sync_data = flow_auth.get_data(url=next_sync_url, token=token)
-    if sync_data:
-        sync(sync_data=sync_data)
-
 
 last_sync_url = crud_sync.get_last_sync(session=session)
 sync_data = None
@@ -156,7 +20,7 @@ if last_sync_url:
         url=last_sync_url.url, token=token)
 
 if sync_data:
-    sync(sync_data=sync_data)
+    data_sync(token=token, session=session, sync_data=sync_data)
 
 
 elapsed_time = time.process_time() - start_time
--- backend/tests/test_010_seed_and_sync.py
+++ backend/tests/test_010_seed_and_sync.py
@@ -7,7 +7,9 @@ from httpx import AsyncClient
 from sqlalchemy.orm import Session
 from seeder.form import form_seeder
 from seeder.datapoint import datapoint_seeder
+from seeder.data_sync import data_sync
 from db import crud_sync
+from db import crud_form
 
 sys.path.append("..")
 pytestmark = pytest.mark.asyncio
@@ -20,7 +22,7 @@ with open(forms_config) as json_file:
     forms = json.load(json_file)
 
 
-class TestSeeder():
+class TestSeedAndSync():
     @pytest.mark.asyncio
     async def test_seed_form_and_data(
         self, app: FastAPI, session: Session, client: AsyncClient
@@ -31,5 +33,22 @@ class TestSeeder():
         sync_res = crud_sync.add_sync(session=session, url=url)
         last_sync = crud_sync.get_last_sync(session=session)
         assert last_sync.url == sync_res.url
+
         form_seeder(session=session, token=token, forms=forms)
         datapoint_seeder(session=session, token=token, forms=forms)
+        for form in forms:
+            fid = form.get('id')
+            check_form = crud_form.get_form_by_id(
+                session=session, id=fid)
+            assert check_form.id == fid
+
+    @pytest.mark.asyncio
+    async def test_sync(
+        self, app: FastAPI, session: Session, client: AsyncClient
+    ) -> None:
+        token = {'token': None, 'time': 0}
+        sync_data_file = "./source/static/sync_data.json"
+        sync_data = {}
+        with open(sync_data_file) as json_file:
+            sync_data = json.load(json_file)
+        data_sync(token=token, session=session, sync_data=sync_data)
