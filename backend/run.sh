4655ded6036a42a6c318ba3ebe710e4725e18ff2
dedenbangkit <mail@dedenbangkit.com>
2022-12-13 13:41:44 +0700

    [#6] Add worker class to gunicorn

--- backend/run.sh
+++ backend/run.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+set -eu
+pip -q install --upgrade pip
+pip -q install --cache-dir=.pip -r requirements.txt
+pip check
+
 if [[ -z "${SKIP_MIGRATION}" ]]; then
     alembic upgrade head
 fi
 
-gunicorn main:app --workers 4 --bind 0.0.0.0:5000
+gunicorn main:app \
+    --workers 4 \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --bind 0.0.0.0:5000
