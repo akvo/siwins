1714519cb804b13b027c8171de3879e5fe86ab8e
dedenbangkit <mail@dedenbangkit.com>
2022-12-13 13:47:47 +0700

    [#6] Remove pip install from runner

--- backend/run.sh
+++ backend/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 set -eu
-pip -q install --upgrade pip
-pip -q install --cache-dir=.pip -r requirements.txt
-pip check
 
 if [[ -z "${SKIP_MIGRATION}" ]]; then
     alembic upgrade head
