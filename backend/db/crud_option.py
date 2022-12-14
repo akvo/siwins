d7efcbdba045050f78d84b60f6a74fffd92f7ae3
ifirmawan <firmawaneiwan@gmail.com>
2022-12-14 15:02:05 +0700

    [#9] add get option by question id

--- backend/db/crud_option.py
+++ backend/db/crud_option.py
@@ -8,9 +8,20 @@ def get_option(session: Session) -> List[OptionDict]:
     return session.query(Option).all()
 
 
+def get_option_by_question_id(
+    session: Session,
+    question=int,
+) -> List[OptionDict]:
+    return session.query(Option).filter(Option.question == question).all()
+
+
 def add_option(
-    session: Session, question=int, name=str,
-    id=Optional[int], order=Optional[str], code: Optional[str] = None
+    session: Session,
+    question=int,
+    name=str,
+    id=Optional[int],
+    order=Optional[str],
+    code: Optional[str] = None,
 ) -> OptionDict:
     question = session.query(Question).filter(Question.id == question).first()
     option = Option(name=name, order=order, code=code)
@@ -22,8 +33,11 @@ def add_option(
 
 
 def update_option(
-    session: Session, id: int, name: Optional[str] = None,
-    order: Optional[str] = None, code: Optional[str] = None,
+    session: Session,
+    id: int,
+    name: Optional[str] = None,
+    order: Optional[str] = None,
+    code: Optional[str] = None,
 ) -> OptionDict:
     option = session.query(Option).filter(Option.id == id).first()
     option.order = order
