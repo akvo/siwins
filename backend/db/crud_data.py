e06df6ae0b031b73d5b80ebdf199ff514ddcafcb
Navin <nikshingote@gmail.com>
2023-01-11 01:37:44 +0530

    [#18] Add query param

--- backend/db/crud_data.py
+++ backend/db/crud_data.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from typing import List, Optional
 from typing_extensions import TypedDict
 from sqlalchemy.orm import Session
-from sqlalchemy import desc, and_, select
+from sqlalchemy import desc, and_, select, or_
 from sqlalchemy.sql.expression import false
 from models.data import Data, DataDict
 from models.answer import Answer
 from models.history import History
 from models.answer import AnswerBase
+from models.advance_filter import ViewAdvanceFilter
 from sqlalchemy.orm import aliased
 
 
@@ -91,8 +92,21 @@ def get_data(
     return PaginatedData(data=data, count=count)
 
 
-def get_all_data(session: Session, registration: bool) -> DataDict:
-    return session.query(Data).filter(Data.registration == registration).all()
+def get_all_data(
+    session: Session,
+    registration: bool,
+    options: List[str] = None,
+) -> DataDict:
+    data = session.query(Data).filter(Data.registration == registration).all()
+    if options:
+        # support multiple select options filter
+        # change query to filter data by or_ condition
+        or_query = or_(
+            ViewAdvanceFilter.options.contains([opt]) for opt in options
+        )
+        data_id = session.query(ViewAdvanceFilter.data).filter(or_query).all()
+        data = data.filter(Data.id.in_([d.data for d in data_id]))
+    return data
 
 
 def get_data_by_id(session: Session, id: int) -> DataDict:
