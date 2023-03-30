2982b58e9dc82cc57e38f490799f464e353b0f26
navins94 <nikshingote@gmail.com>
2023-03-30 17:23:20 +0530

    [#35] Update menu styling

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,41 +1,34 @@
-import { useState } from "react";
-import { PieChartOutlined, DesktopOutlined } from "@ant-design/icons";
-import { Image, Layout, Menu } from "antd";
+import React, { useState } from "react";
+import {
+  PieChartOutlined,
+  DesktopOutlined,
+  MenuUnfoldOutlined,
+} from "@ant-design/icons";
+import { Image, Layout, Menu, theme } from "antd";
 const { Header, Content, Sider } = Layout;
 import "./style.scss";
 
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
 const items = [
-  getItem("Maps", "1", <PieChartOutlined />),
-  getItem("Dashboard", "2", <DesktopOutlined />),
-  getItem("Documentation", "3", <DesktopOutlined />),
+  { label: "Maps", link: "/dashboard", icon: <PieChartOutlined />, key: "1" },
+  { label: "Dashboard", link: "/maps", icon: <DesktopOutlined />, key: "2" },
 ];
+
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout className="dashboard-layout">
-      <Sider
-        width={300}
-        trigger={null}
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-      >
+      <Sider width={300} trigger={null} collapsible collapsed={collapsed}>
         <div className="logo-container">
           <Image src="/images/dashboard-logo.png" preview={false} />
         </div>
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          items={items}
-        />
+        <Menu defaultSelectedKeys={["1"]} mode="inline" className="menu">
+          {items.map((item) => (
+            <Menu.Item key={item.key}>
+              <PieChartOutlined />
+              <span>{item.label}</span>
+            </Menu.Item>
+          ))}
+        </Menu>
       </Sider>
       <Layout className="site-layout">
         <Header />
