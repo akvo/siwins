9ceea593f79bb0ff59f04c35f94b8cc41879357c
wayangalihpratama <wgprtm@gmail.com>
2023-04-12 11:42:10 +0800

    [#44] Update cascade config & option question data type

--- backend/source/cascade_config.py
+++ backend/source/cascade_config.py
@@ -2,7 +2,7 @@ import enum
 
 
 class CascadeTypes(enum.Enum):
-    school_information = 'qid'
+    school_information = 638730933
 
 
 class CascadeLevels(enum.Enum):
@@ -11,4 +11,13 @@ class CascadeLevels(enum.Enum):
         "school_type": 1,
         "school_name": 2,
         "school_code": 3
+    }
+
+
+class CascadeNames(enum.Enum):
+    school_information = {
+        "province": "Province",
+        "school_type": "School Type",
+        "school_name": "School Name",
+        "school_code": "School Code"
     }
