e916c53451f6dc5c741c8014690b18a2d5ce2c69
wayangalihpratama <wgprtm@gmail.com>
2023-04-19 17:03:37 +0800

    [#16] Disable alembic on test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-echo "Migrating main schema"
-alembic upgrade head
+# echo "Migrating main schema"
+# alembic upgrade head
 
 echo "Migrating DB From AkvoResponseGrouper Dependency"
 akvo-responsegrouper --config ./source/category.json
