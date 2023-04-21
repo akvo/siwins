785b07033987cd9bba682f6911671a79bf9719ef
wayangalihpratama <wgprtm@gmail.com>
2023-04-21 20:08:34 +0800

    [#75] Initial JMP chart API

---
+++ backend/db/crud_jmp.py
@@ -0,0 +1,75 @@
+import json
+from sqlalchemy.orm import Session
+from AkvoResponseGrouper.views import get_categories
+from db.crud_data import get_all_data
+
+
+def get_jmp_table_view(session: Session, data: list, configs: list):
+    ids = [str(d["id"]) for d in data]
+    dts = ",".join(ids)
+    try:
+        gc = get_categories(session=session, data=dts)
+    except Exception:
+        gc = []
+    for d in data:
+        cs = list(filter(lambda c: (c["data"] == d["id"]), gc))
+        categories = []
+        for c in cs:
+            labels = get_jmp_labels(configs=configs, name=c["name"])
+            fl = list(
+                filter(
+                    lambda l: l["name"].lower() == str(c["category"]).lower(),
+                    labels,
+                )
+            )
+            color = None
+            if len(fl):
+                color = fl[0]["color"]
+            categories.append(
+                {
+                    "key": c["name"].lower(),
+                    "value": c["category"],
+                    "color": color,
+                }
+            )
+        d.update({"categories": categories})
+    return data
+
+
+def get_jmp_overview(session: Session, name: str):
+    data = get_all_data(session=session, current=True)
+    data = [{
+        "data": d.id,
+        "administration": d.school_information[0],
+        "geo": d.geo
+    } for d in data]
+    try:
+        gc = get_categories(session=session, name=name)
+        for d in data:
+            fc = list(filter(lambda c: (c["data"] == d["data"]), gc))
+            if len(fc):
+                d.update(fc[0])
+        return data
+    except Exception:
+        return data
+
+
+def get_jmp_config_by_form() -> list:
+    try:
+        with open("./.category.json", "r") as categories:
+            json_config = json.load(categories)
+    except Exception:
+        json_config = []
+    configs = json_config
+    return configs
+
+
+def get_jmp_labels(configs: list, name: str) -> list:
+    fl = list(filter(lambda l: l["name"].lower() == name.lower(), configs))
+    labels = []
+    if len(fl):
+        try:
+            labels = fl[0]["labels"]
+        except KeyError:
+            return []
+    return labels
--- backend/routes/charts.py
+++ backend/routes/charts.py
@@ -1,6 +1,7 @@
+from collections import defaultdict
 from fastapi import APIRouter, Query
 from fastapi import Depends, Request
-
+from itertools import groupby
 from typing import List, Optional
 from sqlalchemy.orm import Session
 from db.connection import get_session
@@ -11,9 +12,50 @@ from AkvoResponseGrouper.utils import (
     group_by_category_output,
 )
 from db.crud_data import get_all_data
+from db.crud_jmp import (
+    get_jmp_overview,
+    get_jmp_config_by_form,
+    get_jmp_labels
+)
 
 
 charts_route = APIRouter()
+
+
+def group_children(data, labels):
+    total = len(data)
+    childs = []
+    if not labels:
+        categories = []
+        category = groupby(data, key=lambda d: d["category"])
+        for k, values in category:
+            if k in categories:
+                continue
+            categories.append(k)
+        labels = [{"name": c} for c in categories]
+    groups = groupby(data, key=lambda d: d["administration"])
+    counter = defaultdict()
+    for k, values in groups:
+        print(k)
+        for v in list(values):
+            if v["category"] in list(counter):
+                counter[v["category"]] += 1
+            else:
+                counter[v["category"]] = 1
+    for lb in labels:
+        label = lb.get("name")
+        count = counter[label] if label in counter else 0
+        percent = count / total * 100 if count > 0 else 0
+        childs.append(
+            {
+                "option": label,
+                "count": count,
+                "percent": percent,
+                "color": lb.get("color"),
+            }
+        )
+    print(childs)
+    return {"administration": 'a', "score": 0, "child": childs}
 
 
 @charts_route.get(
@@ -39,3 +81,23 @@ def get_bar_charts(
     df = transform_categories_to_df(categories=categories)
     dt = get_counted_category(df=df)
     return group_by_category_output(data=dt)
+
+
+@charts_route.get(
+    "/chart/jmp-data/{type:path}",
+    name="charts:get_aggregated_jmp_chart_data",
+    summary="get jmp chart aggregate data",
+    tags=["Charts"],
+)
+def get_aggregated_jmp_chart_data(
+    req: Request,
+    type: str,
+    # q: Optional[List[str]] = Query(None),
+    session: Session = Depends(get_session),
+):
+    # options = check_query(q) if q else None
+    data = get_jmp_overview(session=session, name=type)
+    configs = get_jmp_config_by_form()
+    labels = get_jmp_labels(configs=configs, name=type)
+    group = group_children(data, labels)
+    # return {"form": form_id, "question": type, "data": group}
