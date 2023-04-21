0eec8f071018ae675f51ae5227580be204ced5ae
wayangalihpratama <wgprtm@gmail.com>
2023-04-21 15:51:50 +0800

    [#74] Create province number answer view

--- backend/utils/functions.py
+++ backend/utils/functions.py
@@ -6,6 +6,7 @@ def refresh_materialized_view_query():
     return text("""
         REFRESH MATERIALIZED VIEW advance_filter;
         REFRESH MATERIALIZED VIEW province_option_answer;
+        REFRESH MATERIALIZED VIEW province_number_answer;
     """)
 
 
