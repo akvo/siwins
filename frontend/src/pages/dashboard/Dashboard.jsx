fb92e3eab95570ed722221e5fc3c588c13352d25
navins94 <nikshingote@gmail.com>
2023-04-27 19:19:06 +0530

    [#82] Add bar chart

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Select } from "antd";
 import { api } from "../../lib";
 import { UIState } from "../../state/ui";
 import ChartVisual from "./components/ChartVisual";
+import { Chart } from "../../components";
 
 const chartConfig = window.dashboardjson?.tabs;
 
 const Dashboard = () => {
-  const { provinceValues } = UIState.useState((s) => s);
+  const { provinceValues, barChartQuestions } = UIState.useState((s) => s);
   const [chartList, setChartList] = useState([]);
+  const [barChartList, setBarChartList] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
   const [data, setData] = useState([]);
   const [pageLoading, setPageLoading] = useState(false);
 
@@ -18,6 +21,10 @@ const Dashboard = () => {
       ?.chartList.flat();
     setChartList(
       chartConfig.find((item) => item.component === "JMP-CHARTS")?.chartList
+    );
+    setBarChartList(
+      chartConfig.find((item) => item.component === "GENERIC-CHART-GROUP")
+        ?.chartList
     );
     setPageLoading(true);
     const apiCall = chartList?.map((chart) => {
@@ -45,6 +52,20 @@ const Dashboard = () => {
     );
   };
 
+  const handleOnChangeQuestionDropdown = (val) => {
+    if (val) {
+      const url = `chart/generic-bar/${val}`;
+      api
+        .get(url)
+        .then((res) => {
+          setBarChartData(res.data);
+        })
+        .catch((e) => console.error(e));
+    } else {
+      setBarChartData([]);
+    }
+  };
+
   return (
     <div id="dashboard">
       <Row className="main-wrapper" align="center">
@@ -64,6 +85,64 @@ const Dashboard = () => {
           ) : (
             <h4>No data</h4>
           )}
+        </Col>
+      </Row>
+      <Row className="bar-chart-wrapper" align="center">
+        <Col span={24} align="center">
+          <div className="header-container">
+            <h3>Bar Chart for Any Indicator</h3>
+          </div>
+        </Col>
+        <Col span={24} align="center">
+          <div className="chart-wrapper">
+            <Select
+              dropdownMatchSelectWidth={false}
+              placement={"bottomLeft"}
+              showSearch
+              allowClear
+              placeholder="Select question"
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={barChartQuestions?.map((q) => ({
+                label: q?.display_name ? q?.display_name : q.name,
+                value: q.id,
+              }))}
+              onChange={(val) => handleOnChangeQuestionDropdown(val)}
+            />
+            {barChartData?.data?.length > 0 &&
+              barChartList?.map((row) => {
+                return (
+                  <Row
+                    key={`row-${row.name}`}
+                    className="flexible-container row-wrapper"
+                    gutter={[10, 10]}
+                  >
+                    <Col span={24}>
+                      <Chart
+                        height={50 * barChartData?.data.length + 188}
+                        type={"BAR"}
+                        dataZoom={false}
+                        data={barChartData?.data.map((v) => ({
+                          name: v.name,
+                          value: v.value,
+                          count: v.value,
+                        }))}
+                        wrapper={false}
+                        horizontal={false}
+                        grid={{
+                          top: 70,
+                          left: 120,
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                );
+              })}
+          </div>
         </Col>
       </Row>
     </div>
--- frontend/src/state/ui.js
+++ frontend/src/state/ui.js
@@ -6,6 +6,7 @@ const defaultUIState = {
   schoolTypeValues: [],
   indicatorQuestions: [],
   advanceFilterQuestions: [],
+  barChartQuestions: [],
   mapData: [],
 };
 
