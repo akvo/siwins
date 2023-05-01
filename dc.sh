1e0ca15fdc18079c1da088cd6926d76dd515d037
dedenbangkit <mail@dedenbangkit.com>
2023-05-01 20:42:11 +0700

    [#92] source, deploy and config override

--- dc.sh
+++ dc.sh
@@ -7,4 +7,10 @@ fi
 
 pre-commit install
 
+if [[ $1 == "up" ]]; then
+rm -rf ./docs/build/html/*
+docker run -it --rm -v "$(pwd)/docs:/docs" \
+		akvo/akvo-sphinx:20220525.082728.594558b make html
+fi
+
 COMPOSE_HTTP_TIMEOUT=180 docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-docker-sync.yml "$@"
