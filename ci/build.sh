70c5b7efd7db0b4c13af7cd0a14d0739c219e89c
dedenbangkit <mail@dedenbangkit.com>
2023-05-01 21:01:50 +0700

    [#92] Fix documentation build

--- ci/build.sh
+++ ci/build.sh
@@ -24,8 +24,8 @@ documentation_build () {
 
 		docker run -it --rm -v "$(pwd)/docs:/docs" \
 				akvo/akvo-sphinx:20220525.082728.594558b make html
-		mkdir -p frontend/build/docs/${INSTANCE}
-		cp -r ./docs/build/html ./frontend/documentation
+		mkdir -p frontend/build/docs/
+		cp -r ./docs/build/html ./frontend/build/documentation
 
 }
 
