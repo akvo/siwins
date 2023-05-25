4d140792337729a5fee366ae2499adc6f8a4096e
wayangalihpratama <wgprtm@gmail.com>
2023-05-25 15:17:39 +0800

    [#139] Refactor data detail pop up test case

--- backend/tests/test_101_data_detail_popup.py
+++ backend/tests/test_101_data_detail_popup.py
@@ -3,9 +3,9 @@ import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
 from sqlalchemy.orm import Session
-from tests.test_school_detail_popup_dump import (
-    res_school_detail_popup
-)
+# from tests.test_school_detail_popup_dump import (
+#     res_school_detail_popup
+# ) # TODO:: Delete
 
 sys.path.append("..")
 pytestmark = pytest.mark.asyncio
@@ -20,7 +20,26 @@ class TestDataDetailRoutes:
             app.url_path_for("data:get_data_detail", data_id=649130936))
         assert res.status_code == 200
         res = res.json()
-        assert res == res_school_detail_popup
+        assert list(res) == [
+            'id', 'name',
+            'year_conducted', 'school_information',
+            'jmp_levels', 'answer'
+        ]
+        assert list(res["jmp_levels"][0]) == [
+            'year', 'history', 'category', 'level', 'color'
+        ]
+        assert list(res["answer"][0]) == ['group', 'child']
+        assert list(res["answer"][0]["child"][0]) == [
+            'question_id', 'question_name',
+            'render', 'history', 'year', 'value'
+        ]
+        for it in list(res["answer"][0]["child"]):
+            if it["render"] == "chart":
+                assert list(it["value"][0]) == [
+                    'level', 'total', 'count', 'value'
+                ]
+        # TODO:: Delete
+        # assert res == res_school_detail_popup
 
     @pytest.mark.asyncio
     async def test_get_answer_history(
@@ -47,27 +66,36 @@ class TestDataDetailRoutes:
             params={'question_id': 624670928})
         assert res.status_code == 200
         res = res.json()
-        assert res == [{
-            'question_id': 624670928,
-            'question_name': 'No. of classrooms',
-            'type': 'number',
-            'history': True,
-            'year': 2018,
-            'value': [{
-                'level': 'AO CHS - 21710',
-                'total': 12.0,
-                'count': 1,
-                'value': 12.0,
-            }, {
-                'level': 'Guadalcanal',
-                'total': 12.0,
-                'count': 1,
-                'value': 12.0,
-            }, {
-                'level': 'National',
-                'total': 12.0,
-                'count': 1,
-                'value': 12.0,
-            }],
-            'render': 'chart'
-        }]
+        assert list(res[0]) == [
+            'question_id', 'question_name', 'type',
+            'history', 'year', 'value', 'render'
+        ]
+        assert res[0]["history"] is True
+        assert list(res[0]["value"][0]) == [
+            'level', 'total', 'count', 'value'
+        ]
+        # TODO:: Delete
+        # assert res == [{
+        #     'question_id': 624670928,
+        #     'question_name': 'No. of classrooms',
+        #     'type': 'number',
+        #     'history': True,
+        #     'year': 2018,
+        #     'value': [{
+        #         'level': 'AO CHS - 21710',
+        #         'total': 12.0,
+        #         'count': 1,
+        #         'value': 12.0,
+        #     }, {
+        #         'level': 'Guadalcanal',
+        #         'total': 12.0,
+        #         'count': 1,
+        #         'value': 12.0,
+        #     }, {
+        #         'level': 'National',
+        #         'total': 12.0,
+        #         'count': 1,
+        #         'value': 12.0,
+        #     }],
+        #     'render': 'chart'
+        # }]
