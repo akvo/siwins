eb551d9d04510f54a91315a52a03177d178f27e6
wayangalihpratama <wgprtm@gmail.com>
2023-08-11 17:54:07 +0800

    [#165] Remap prod question

--- backend/source/remap_form_definition.py
+++ backend/source/remap_form_definition.py
@@ -1,6 +1,6 @@
 import json
 
-prev_file = "./source/forms/647170919.json"
+prev_file = "./source/forms/test_647170919.json"
 new_file = "./source/new_form.json"
 
 with open(prev_file, 'r') as json_file:
@@ -21,17 +21,28 @@ for qgi, qg in enumerate(new_qg):
 
     for qi, q in enumerate(qg['question']):
         qid = q['id']
+        qname = q['text']
         if "Q" in qid:
             qid = qid[1:]
         find_prev_q = list(filter(
-            lambda obj: obj["id"] == qid,
+            lambda obj: obj["id"] == qid or (
+                obj["text"].lower() == qname.lower()
+            ),
             prev_question
-        ))[0]
+        ))
+        if find_prev_q:
+            find_prev_q = find_prev_q[0]
+        if not find_prev_q:
+            try:
+                find_prev_q = prev_question[qi]
+            except Exception as e:
+                find_prev_q = None
+                print(qid, e)
         # remap
         q["id"] = qid
-        if "attributes" in find_prev_q:
+        if find_prev_q and "attributes" in find_prev_q:
             q["attributes"] = find_prev_q["attributes"]
-        if "displayName" in find_prev_q:
+        if find_prev_q and "displayName" in find_prev_q:
             q["displayName"] = find_prev_q["displayName"]
         # eol of remap
 
