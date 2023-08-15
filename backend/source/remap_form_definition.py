b34e038944fd2d01bf08c2289b8d2a75d673c76b
wayangalihpratama <wgprtm@gmail.com>
2023-08-15 16:34:44 +0800

    [#165] Update prod form to v21

--- backend/source/remap_form_definition.py
+++ backend/source/remap_form_definition.py
@@ -1,6 +1,6 @@
 import json
 
-prev_file = "./source/forms/test_647170919.json"
+prev_file = "./source/forms/634200917.json"
 new_file = "./source/new_form.json"
 
 with open(prev_file, 'r') as json_file:
@@ -32,12 +32,13 @@ for qgi, qg in enumerate(new_qg):
         ))
         if find_prev_q:
             find_prev_q = find_prev_q[0]
-        if not find_prev_q:
-            try:
-                find_prev_q = prev_question[qi]
-            except Exception as e:
-                find_prev_q = None
-                print(qid, e)
+        # find prev q by index to remap test - prod form
+        # if not find_prev_q:
+        #     try:
+        #         find_prev_q = prev_question[qi]
+        #     except Exception as e:
+        #         find_prev_q = None
+        #         print(qid, e)
         # remap
         q["id"] = qid
         if find_prev_q and "attributes" in find_prev_q:
